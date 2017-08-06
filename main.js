a5f342350d3ec377b472c60f57033e4c07bf8d34
Emily Boucher <emi.boucher@gmail.com>
2017-08-05 23:10:36 -0400

    added event listener for border, additional absolute positioning added to header to account for event

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-var subTitle =document.querySelector("#subtitle");
-var titlePage = document.querySelector("#title-page");
+  var subTitle =document.querySelector("#subtitle");
+
+  var titlePage = document.querySelector("#title-page");
 
   function showSubTitle() {
     subTitle.style.visibility = "visible";
   };
 
-  // function changeTitleBack() {
-  //   titlePage.innerText = "Web Developer";
-  // }
+  function addSubTitleBorder() {
+    subTitle.style.border = "2px solid black";
+  };
 
   titlePage.addEventListener("mouseover", showSubTitle);
 
+  subTitle.addEventListener("mouseover", addSubTitleBorder);
 
 });
